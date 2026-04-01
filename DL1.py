22027f0dd7aae37e556dd50996f95cea4257fa8e
VIKAS YADAV <yadavikas088@gmail.com>
2026-04-01 20:43:42 +0530

    Add matrix multiplication and eigenvalue computation

---
+++ DL1.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+print('Matrix Multiplication Demo')
+x = tf.constant([[1, 2, 3],
+[4, 5, 6]], dtype=tf.float32)
+print ('Matrix X:\n', x)
+y = tf.constant([[7,8],
+[9,10],
+[11, 12]], dtype=tf.float32)
+print ("Matrix Y:\n", y)
+z = tf.matmul(x,y)
+print ("Product (x x Y):\n", z)
+
+# --- Eigenvalues and Elgenvectors —-
+A = tf.constant([[1, 2],
+[5, 4]], dtype=tf.float32)
+print("\nMatrix A:\n", A)
+e_vals, e_vecs = tf.linalg.eigh(A)
+print("\nEigenvalues:\n", e_vals)
+print("\nEigenvectors:\n", e_vecs)
